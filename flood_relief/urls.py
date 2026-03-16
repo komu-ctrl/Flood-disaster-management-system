6ce7667e1a510138a8fa45239694e23dd73c1ef5
komu-ctrl <komudennise@gmail.com>
2026-03-16 20:38:52 +0300

    Added application feature

--- flood_relief/urls.py
+++ flood_relief/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('floods.urls')),
-    path('relief/', include('myapp.urls')),
+    path('', include('myapp.urls')),
 ]
